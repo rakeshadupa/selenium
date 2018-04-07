4b62a47385828fd237bcad54c5528a97e72e6a49
rakesh <rakeshadupa931@gmail.com>
2018-04-07 10:49:02 +0530

    demo code

--- src/main/java/com/maven/sample/Practice.java
+++ src/main/java/com/maven/sample/Practice.java
@@ -7,16 +7,30 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
+
+
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-public interface Practice {
-
-	 void test(); 
+public class Practice extends SeleniumBase{
+@Test
+	 public void testPractice() throws InterruptedException
+	 {
+		 driver.get("http://192.168.2.60/ebadi_web/public/");
+//		 driver.findElement(By.partialLinkText("sign")).click();
+		 driver.findElement(By.xpath("//a[contains(text(),'sign')]")).click();
+		 driver.findElement(By.id("username")).sendKeys("rakesh");
+		 driver.findElement(By.id("email")).sendKeys("rakeshraki@gmail.com");
+		 driver.findElement(By.id("pass")).sendKeys("1234567");
+		 Select s=new Select(driver.findElement(By.id("state")));
+		 s.selectByIndex(1);
+		 driver.findElement(By.id("login_btn")).click();
+		 Thread.sleep(3000);
+		 
+	 }
 		
 
 
