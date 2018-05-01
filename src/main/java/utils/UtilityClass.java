7f1150f568ed18da7c4ddb343f841704b55d3921
rakesh <rakeshadupa96@gmail.com>
2018-05-01 15:02:12 +0530

    test

---
+++ src/main/java/utils/UtilityClass.java
@@ -0,0 +1,23 @@
+package utils;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class UtilityClass {
+
+	public static void takeScreenshot(WebDriver driver) throws IOException {
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentPath = System.getProperty("user.dir");
+		FileUtils.copyFile(src, new File(currentPath + "/screenshots/" + System.currentTimeMillis() + ".png"));
+		//
+		
+		
+	
+		
+	}
+}
