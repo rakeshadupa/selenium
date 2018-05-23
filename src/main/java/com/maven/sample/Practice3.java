8a9cf1d61bf33101b146bb1dc6d67c816d2e4532
rakesh <rakeshadupa96@gmail.com>
2018-05-23 09:51:37 +0530

    test

--- src/main/java/com/maven/sample/Practice3.java
+++ src/main/java/com/maven/sample/Practice3.java
@@ -2,11 +2,11 @@ package com.maven.sample;
 
 import org.testng.annotations.Test;
 
-public class Practice3 {
+public class Practice3 extends SeleniumBase {
 
 	@Test
 	public void demo() throws Exception {
-
-
+		driver.get("https://stackoverflow.com/questions/11947832/how-to-click-an-element-in-selenium-webdriver-using-javascript");
 	}
+
 }
