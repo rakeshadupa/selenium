4bff7e7cd23a7716b373ac67db7ff3e3cde192e8
rakesh <rakeshadupa96@gmail.com>
2018-07-26 18:56:00 +0530

    final

--- src/main/java/com/maven/sample/Practice1.java
+++ src/main/java/com/maven/sample/Practice1.java
@@ -3,11 +3,26 @@ package com.maven.sample;
 import java.awt.AWTException;
 
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class Practice1 {
+	boolean a = true;
 
 	@Test
-	public void run(String a, int b) throws InterruptedException, AWTException {
+	public void run() throws InterruptedException, AWTException {
+
+		SoftAssert s = new SoftAssert();
+		s.assertTrue(true);
+		s.assertTrue(a);
+	
+		try {
+		s.assertAll();
+		}
+		catch(AssertionError e)
+		{
+		System.err.println("extent  fail log here"); 
+		}
 
 	}
 }
+
