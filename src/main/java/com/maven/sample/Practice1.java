9d0d7bdae72c292d295c1f857f0a5d43e1f0c4bc
rakesh <rakeshadupa931@gmail.com>
2018-09-20 16:29:53 +0530

    go

--- src/main/java/com/maven/sample/Practice1.java
+++ src/main/java/com/maven/sample/Practice1.java
@@ -3,14 +3,12 @@ package com.maven.sample;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
-public class Practice1 extends SeleniumBase {
+public class Practice1  {
 	@Test
 	public void testDemo() {
+System.out.println("rakesh");
 
 		
 		
-		driver.get("https://www.troopcrm.com");
-		driver.findElement(By.id("username")).sendKeys("123456");
-		System.err.println("text"+driver.findElement(By.id("username")).getAttribute("value"));
 	}
 }
